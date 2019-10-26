ee1576de10321b4fba441ca80f9b821633481b58
unknown <jinyoung777@gmail.com>
2019-10-26 14:45:05 +0900

    Backend Users, Contacts & JWT Auth

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,51 @@
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 
 // @route   POST /api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-    res.send('Register a user');
+router.post('/', [
+    check('name').not().isEmpty(),
+    check('email').isEmail(),
+    check('password').isLength({ min: 6 })
+], async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, password } = req.body;
+
+    try {
+        let user = await User.findOne({email: email});
+        if (user) {
+            return res.status(400).json({msg: "User already exists."})
+        }
+
+        user = new User({name, email, password});
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+        //res.send('User saved');
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 360000 }, (err, token) => {
+            if (err) throw err;
+            res.json({token});
+        });
+    } catch (err) {
+        res.status(500).send('Server error');
+        console.log(err.message);
+    }
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect DB
+connectDB();
+
+// Init middleware
+app.use(express.json({extended: false}));
+app.get('/', (req, res) => res.json({msg: 'Welcome to the ContactKeeper API...'}));
 
 // Define Routes
 app.use('/api/users', require('./routes/users'));
