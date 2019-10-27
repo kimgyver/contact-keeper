dbb6fbc6069b3662d3f372d064a3acdb783f252f
unknown <jinyoung777@gmail.com>
2019-10-27 16:21:51 +0900

    animation effect using TransitionGroup

--- client/src/components/contacts/Contacts.js
+++ client/src/components/contacts/Contacts.js
@@ -1,4 +1,5 @@
 import React, {Fragment, useContext} from 'react';
+import {CSSTransition, TransitionGroup} from 'react-transition-group';
 import ContactContext from '../../context/contact/contactContext';
 import ContactItem from './ContactItem';
 
@@ -15,10 +16,14 @@ const Contacts = () => {
 
     return (
         <Fragment>
+            <TransitionGroup>
             {
                 contactsForHere.map(contact => (
-                <ContactItem key={contact.id} contact={contact}/>
+                    <CSSTransition key={contact.id} timeout={500} classNames='item'>
+                        <ContactItem contact={contact}/>
+                    </CSSTransition>
             ))}
+            </TransitionGroup>
         </Fragment>
     );
 }
