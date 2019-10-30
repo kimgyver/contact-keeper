1562cf558633abef9ee49046573bd8c4ef8daeb0
unknown <jinyoung777@gmail.com>
2019-10-30 18:15:54 +0900

    heroku environment

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = reqire('path');
 
 const app = express();
 var cors = require('cors');
@@ -25,6 +26,14 @@ app.use('/api/contacts', require('./routes/contacts'));
 
 // Add corsOptions to express app
 app.use(cors(corsOptions));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')));
+}
 
 const PORT = process.env.PORT || 5000;
 
